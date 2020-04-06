566462ef735fcf7565e275e79a871141aed65b7f
busy0312 <abbymacbook@Abbys-MacBook-Pro.local>
2020-04-06 15:49:01 -0500

    updated

--- Instructions/EmployeeSQL/schemas.sql
+++ Instructions/EmployeeSQL/schemas.sql
@@ -1,7 +1,4 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/vospTr
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
+﻿
 CREATE TABLE departments (
     dept_no char(4) Primary Key  NOT NULL,
     dept_name varchar   NOT NULL
